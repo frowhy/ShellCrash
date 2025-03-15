3aa2f7fbe9362efecfaa23952ce7a43cdc60565f
Make China Great Again <frowhy@gmail.com>
2025-03-15 19:13:06 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 # Copyright (C) Juewuy
 
-[ -z "$url" ] && url="https://fastly.jsdelivr.net/gh/juewuy/ShellCrash@master"
+[ -z "$url" ] && url="https://ghproxy.cfd/https://raw.githubusercontent.com/frowhy/ShellCrash/dev"
 type bash &>/dev/null && shtype=bash || shtype=sh
 echo='echo -e'
 [ -n "$(echo -e | grep e)" ] && {
